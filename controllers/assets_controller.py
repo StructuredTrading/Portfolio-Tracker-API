d0ef0197a3b8556298f6353a141f9bbbe277fa21
StructuredTrading <samowen1992@hotmail.com>
2024-03-07 20:57:09 +1000

    Added CRUD operation to search 'assets' by 'assetID' with '/assets/search/<asset_id>'.

--- controllers/assets_controller.py
+++ controllers/assets_controller.py
@@ -13,3 +13,12 @@ def retrieve_all_assets():
     stmt = db.select(Asset)
     assets = db.session.execute(stmt).scalars().all() 
     return assets_schema.dump(assets)
+
+
+# Retrieve asset by assetID
+@assets_bp.route("/search/<asset_id>")
+def search_assets(asset_id):
+    # retrieve asset with 'asset_id' from the database
+    stmt = db.select(Asset).filter_by(assetID = asset_id)
+    assets = db.session.scalar(stmt)
+    return asset_schema.dump(assets)
