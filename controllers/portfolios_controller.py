9495d5789fe5425719dbdf16fe6889add5b6bd3b
StructuredTrading <samowen1992@hotmail.com>
2024-03-07 15:52:11 +1000

    Added CRUD operation to update a portfolio in portfolios table by the owner of the portfolio.

--- controllers/portfolios_controller.py
+++ controllers/portfolios_controller.py
@@ -10,7 +10,7 @@ from models.portfolios import Portfolio, portfolio_schema, portfolios_schema
 portfolios_bp = Blueprint("portfolios", __name__, url_prefix="/portfolios")
 
 # Retrieve all portfolios from portfolios table in database
-@portfolios_bp.route("/")
+@portfolios_bp.route("/") # /portfolios
 def get_all_portfolios():
     # SQL select statement to retrieve all entries from 'portfolios' table,
     # ordering them by 'portfolioID'
@@ -21,7 +21,7 @@ def get_all_portfolios():
 
 
 # Create new portfolio in portfolios table
-@portfolios_bp.route("/create", methods=["POST"])
+@portfolios_bp.route("/create", methods=["POST"]) # /portfolios/create
 @jwt_required()
 def create_portfolio():
     # Retrieve body data from JSON
@@ -38,3 +38,29 @@ def create_portfolio():
     db.session.commit()
     # Return newly created portfolio
     return portfolio_schema.dump(portfolio), 201
+
+
+# Update a portfolio in portfolios table
+@portfolios_bp.route("/update/<int:portfolio_id>", methods=["PUT","PATCH"])
+@jwt_required()
+def update_portfolio(portfolio_id):
+    # Retrieve body data from JSON
+    data = portfolio_schema.load(request.get_json())
+    # Retrieve the portfolio from database whose fields need to be updated
+    stmt = db.select(Portfolio).filter_by(portfolioID=portfolio_id)
+    portfolio = db.session.scalar(stmt)
+    # If portfolio exists
+    if portfolio:
+        if str(portfolio.userID) != get_jwt_identity():
+            return {"error": "Only the owner can edit this portfolio"}, 403
+        # Update the fields
+        portfolio.name=data.get("name") or portfolio.name,
+        portfolio.description=data.get("description") or portfolio.description
+        # commit the changes
+        db.session.commit()
+        # Return the updated portfolio back
+        return portfolio_schema.dump(portfolio)
+    # Else
+    else:
+        # Return error msg
+        return {"error": f"Portfolio with id '{portfolio_id}' not found"}, 404
